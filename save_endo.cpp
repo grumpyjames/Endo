e371d242c4dbfacf542eaa1cf9a3dab7cf26be7e
grumpyjames <grumpyjames@gmail.com>
2009-09-15 12:26:38 +0300

    corrections to call to match replace, most of the match replace procedure without error checking

--- save_endo.cpp
+++ save_endo.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cmath>
+#include <deque>
+#include <list>
 using std::string;
 using std::ifstream;
 namespace {
@@ -229,6 +232,70 @@ string make_template(string & dna, string & rna)
   throw finish_exception("ran out of dna!");
 }
 
+string cmake_template(char *& dna, string & rna)
+{
+  string result_pattern;
+  string rescuable;
+  while (strlen(dna)>0) {
+    char first = dna[0];
+    ++dna;
+    switch (first) {
+    case 'C':
+      result_pattern.push_back('I');
+      break;
+    case 'F':
+      result_pattern.push_back('C');
+      break;
+    case 'P':
+      result_pattern.push_back('F');
+      break;
+    case 'I':
+      char second = dna[0];
+      ++dna;
+      switch (second) {
+      case 'C':
+	result_pattern.push_back('P');
+	break;
+      default:
+	{
+	  if (second=='F' || second=='P') {
+	    string l = cnat(dna, rescuable);
+	    string n = cnat(dna, rescuable);
+	    result_pattern.push_back('[');
+	    result_pattern.append(n);
+	    result_pattern.push_back('_');
+	    result_pattern.append(l);
+	    result_pattern.push_back(']');
+	  }
+	  else { //char=='I'
+	    char third = dna[0];
+	    ++dna;
+	    switch (third) {
+	    case 'I':
+	      {
+		cpush_dna_to_rna(dna, rna);
+	      }
+	      break;
+	    case 'P':
+	      {
+		string n = cnat(dna, rescuable);
+		result_pattern.push_back('|');
+		result_pattern.append(n);
+		result_pattern.push_back('|');
+	      }
+	      break;
+	    default: //C or F
+	      return result_pattern;
+	    }
+	  }
+	  break;
+	}
+      }
+    }
+  }
+  throw finish_exception("ran out of dna!");
+}
+
 string pattern(char *& dna, string & rna) //evil, but I'm lazy...
 {
   string result_pattern(""), rescuable("");
@@ -262,6 +329,7 @@ string pattern(char *& dna, string & rna) //evil, but I'm lazy...
 	  string s = cconsts(dna, rescuable);
 	  result_pattern.push_back('?');
 	  result_pattern.append(s);
+	  result_pattern.push_back('/');
 	}
 	  break;
       case 'P':
@@ -269,6 +337,7 @@ string pattern(char *& dna, string & rna) //evil, but I'm lazy...
 	  string n = cnat(dna, rescuable);
 	  result_pattern.push_back('!');
 	  result_pattern.append(n); //we don't want to add ! if finish exception is thrown.
+	  result_pattern.push_back('/'); //easier to find a terminator
 	}
 	break;
       case 'I':
@@ -325,6 +394,160 @@ string pattern(char *& dna, string & rna) //evil, but I'm lazy...
 
  */
 
+void asnat(unsigned int k, std::string & to_write_to)
+{
+  if (k==0) {
+    to_write_to.push_back('P');
+    return;
+  }
+  else if (k & 1) {
+    to_write_to.push_back('I');
+    asnat(floor(k/2), to_write_to);
+  }
+  else {
+    to_write_to.push_back('C');
+    asnat(floor(k/2), to_write_to);
+  }
+}
+
+void protect(unsigned int level, string & bits, string & to_write_to)
+{
+  for (unsigned int i=0; i<=level; ++i) {
+    switch (bits[i]) {
+    case 'I':
+      to_write_to.push_back('C');
+      break;
+    case 'C':
+      to_write_to.push_back('F');
+      break;
+    case 'F':
+      to_write_to.push_back('P');
+      break;
+    case 'P':
+      to_write_to.append("IC");
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+
+void replace(char * dna, string & a_template, std::deque<string> & env)
+{
+  string r("");
+  for (unsigned int i=0; i<a_template.length(); ++i) {
+    switch (a_template[0]) {
+    case '[':
+      {
+	++i;
+	size_t found = a_template.find(',', i) - 1;
+	unsigned int n;
+	std::istringstream a_stream(a_template.substr(i, found-i+1));
+	a_stream >> n;
+	i = ++found;
+	found = a_template.find(']', i) - 1;
+	unsigned int l;
+	std::istringstream b_stream(a_template.substr(i, found-i+1));
+	b_stream >> l;
+	i = found;
+	string to_append("");
+	protect(l, env[n], to_append);
+	r.append(to_append);
+      }
+      break;
+    case '|':
+      {
+	++i;
+	size_t found = a_template.find('|', i) - 1;
+	unsigned int n;
+	std::istringstream a_stream(a_template.substr(i, found));
+	a_stream >> n;
+	i = ++found; //should move us to the final |
+	string to_append;
+	asnat(env[n].length(), to_append);
+	r.append(to_append);
+      }
+      break;
+    default: //lone base
+      r.push_back(a_template[0]);
+      break;
+    }
+  }
+  std::cout << "r was " << r << '\n';
+}
+
+void match_replace(char *& dna, string pattern, string a_template)
+{
+  unsigned int i(0);
+  std::list<unsigned int> c;
+  std::deque<string> env;
+  //std::cout << strlen(dna) << " - dna length" << '\n';
+  for (size_t s=0; s<pattern.length(); ++s) {
+    switch (pattern[s]) {
+    case '!':
+      {
+	++s;
+	size_t end_of_nat = pattern.find('/', s) - 1;
+	string n = pattern.substr(s,end_of_nat-s+1);
+	//std::cout << "found nat: " << n << " end of nat " << end_of_nat << " s " << s << '\n';
+	s = end_of_nat;
+	i += atoi(n.c_str()); //evil, but probably quicker than streams: TEST!
+      }
+      break;
+    case '?':
+      {
+	++s;
+	size_t end_of_consts = pattern.find('/',s) - 1;
+	string n = pattern.substr(s,end_of_consts-s+1);
+	char * found = strstr(dna, n.c_str());
+	if (found == NULL)
+	  return;
+	else
+	  i = strlen(dna)-strlen(found);
+	break;
+      }
+    case '(':
+      c.push_front(i);
+      break;
+    case ')':
+      {
+	//std::cout << "Closing a brace" << '\n';
+	string building("");
+	//std::cout << "Instantiating a string" << '\n';
+	unsigned int moo = c.front();
+	building.reserve(i-moo);
+	//std::cout << "DNA length: " << strlen(dna) << ". Copying " << (i - moo) << " chars to building from position " << c.front() << '\n';
+	for (; moo<=i; ++moo) {
+	  //std::cout << dna[moo] << '\n';
+	  building.push_back(dna[moo]);
+	}
+	//std::cout << "done it, building is now " << building << '\n';
+	env.push_back(building);
+	//std::cout << "Adding the building to the env" << '\n';
+	c.pop_front();
+	//std::cout << "Removing the position from the list" << '\n';
+	break;
+      }
+    case '/':
+      break;
+    default: //I C F or P
+      if (pattern[s]==dna[i])
+	++i;
+      else
+	return;
+      break;
+    }
+  }
+  std::cout << "successful match: i= " << i << '\n'; 
+  dna+=i;
+  std::cout << "env[0] length is " << env[0].length() << '\n';
+  std::cout << "env[1] length is " << env[1].length() << '\n';
+  replace(dna, a_template, env);
+}
+
+
+
 int main(int argc, char* argv[])
 {
  
@@ -345,7 +568,7 @@ int main(int argc, char* argv[])
   rna = "";
   string our_pattern = pattern(cdna,rna);
   std::cout << our_pattern << '\n';
-  assert ("(!2)P"==our_pattern);
+  assert ("(!2/)P"==our_pattern);
 
   //now do the real thing:
   ifstream ifs( "endo.dna" );
@@ -357,14 +580,13 @@ int main(int argc, char* argv[])
   string pattern_holder(""),template_holder(""); 
   char * primitive_dna = const_cast<char *>(actual_dna.c_str());
   try {
-    for (;;){
-      pattern_holder = pattern(primitive_dna, actual_rna);
-      std::cout << "pattern: " << pattern_holder << '\n';
-      std::cout << "pattern length was " << pattern_holder.length() << '\n';
-      template_holder = make_template(actual_dna, actual_rna);
-      std::cout << "template: " << template_holder << '\n';
-      std::cout << "rna length: " << actual_rna.length() << '\n';
-    }
+    pattern_holder = pattern(primitive_dna, actual_rna);
+    std::cout << "pattern: " << pattern_holder << '\n';
+    std::cout << "pattern length was " << pattern_holder.length() << '\n';
+    template_holder = cmake_template(primitive_dna, actual_rna);
+    std::cout << "template: " << template_holder << '\n';
+    std::cout << "rna length: " << actual_rna.length() << '\n';
+    match_replace(primitive_dna, pattern_holder, template_holder);
   }
   catch (finish_exception & fe)
     {
